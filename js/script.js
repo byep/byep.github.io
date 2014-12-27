dd504d6aefabda193322d477ebe8021284f63001
byep <byep@risd.edu>
2014-12-26 18:38:44 -0700

    test
    test

--- js/script.js
+++ js/script.js
@@ -1,97 +1,36 @@
-//_____*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*______SLIDING MENU________*_*_*_*_*_*_*_*_*
-
-var shakemore = 0
-
 $(document).ready(function() {
-
-
-
-
-var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
-        menuRight = document.getElementById( 'cbp-spmenu-s2' ),
-        menuTop = document.getElementById( 'cbp-spmenu-s3' ),
-        menuBottom = document.getElementById( 'cbp-spmenu-s4' ),
-        showLeft = document.getElementById( 'showLeft' ),
-        showRight = document.getElementById( 'showRight' ),
-        showTop = document.getElementById( 'showTop' ),
-        showBottom = document.getElementById( 'showBottom' ),
-        showLeftPush = document.getElementById( 'showLeftPush' ),
-        showRightPush = document.getElementById( 'showRightPush' ),
-        body = document.body;
- 
-
-showTop.onclick = function() {
-    classie.toggle( this, 'active' );
-    classie.toggle( menuTop, 'cbp-spmenu-open' );
-    disableOther( 'showTop' );
-};
-
- 
-function disableOther( button ) {
-    if( button !== 'showLeft' ) {
-        classie.toggle( showLeft, 'disabled' );
-    }
-    if( button !== 'showRight' ) {
-        classie.toggle( showRight, 'disabled' );
-    }
-    if( button !== 'showTop' ) {
-        classie.toggle( showTop, 'disabled' );
-    }
-    if( button !== 'showBottom' ) {
-        classie.toggle( showBottom, 'disabled' );
-    }
-    if( button !== 'showLeftPush' ) {
-        classie.toggle( showLeftPush, 'disabled' );
-    }
-    if( button !== 'showRightPush' ) {
-        classie.toggle( showRightPush, 'disabled' );
-    }
-}
-   
-});
-
-//_____*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*______HIDE ABOUT________*_*_*_*_*_*_*_*_*
-$(document).ready(function(){
-  
-    $(".blurred").hide()
-    $(".black_cover").hide();
-    $(".about_text").hide();
-    $(".blank").hide();
     
 
-  $(".move4").click(function(){
-    $(".blurred").fadeToggle("slow", "linear");
-    $(".black_cover").fadeToggle("slow", "linear");
-    $(".about_text").fadeToggle("slow", "linear");
-    // $('.about_text').foggy({blurRadius: 90});
-  });
 
-  $(".about_text").click(function(){
-    $(".blurred").fadeToggle("slow", "linear");
-    $(".black_cover").fadeToggle("slow", "linear");
-    $(".about_text").fadeToggle("slow", "linear");
-    // $('.about_text').foggy({blurRadius: 90});
-  });
+  // $("img").hover(function(){
+  //   $(".background").fadeIn("fast", "linear");
+  // });
 
 
-    $(".blurred").show();
-    $(".black_cover").show();
-    $(".about_text").show();
-    $(".blank").show();
 
-$(".blank").click(function(){
-    $(".blurred").fadeOut("slow", "linear");
-    $(".black_cover").fadeOut("slow", "linear");
-    $(".about_text").fadeOut("slow", "linear");
-    $( ".blank" ).detach();
-    });
+//___*_*_*_*_*_*______HOVER BACKGROUND________*_*_*_*_*_*_*_*_*
 
-$(".move2").click(function(){
-    $(".blurred").fadeOut("slow", "linear");
-    $(".black_cover").fadeOut("slow", "linear");
-    $(".about_text").fadeOut("slow", "linear");
-    $( ".blank" ).detach();
-    });
+$(".folder_x0y0").bind("mouseenter",function(){
+   $.backstretch("images/comraes4.jpg");
+   $( ".reset" ).hide();
+});
+
+$(".folder_x1y0").bind("mouseenter",function(){
+   $.backstretch("images/window_flower8.jpg");
+   $( ".reset" ).hide();
+});
+
+
+
+//___*_*_*_*_*_*______CONTROLS________*_*_*_*_*_*_*_*_*
+
+$( ".reset" ).hide();
+
+
+$(".reset_command").click(function(){
+   $(".reset").show();
+});
+
 
 });
 
